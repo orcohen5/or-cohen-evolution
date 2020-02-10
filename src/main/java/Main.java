54153120f388581a8e4d063d815da16276cfd81a
אור מרד כהן <s8253446@idf.il>
2020-02-10 16:42:23 +0200

    feat creating Main class which creates the instances of tribes, continents and planet and runs the planet life cycle.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
+import Organisms.*;
+import Places.Continent;
+import Places.Planet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("second");
+        Homosepian homosepian = new Homosepian("Nadav");
+        Neanderthal neanderthal = new Neanderthal("Yehuda");
+        Homoerectus homoerectus = new Homoerectus("Tal");
+        Homoebolis homoebolis = new Homoebolis("Sapir");
+        Homofloresiensis homofloresiensis = new Homofloresiensis("Or");
+        Continent asia = new Continent("Asia");
+        Continent africa = new Continent("Africa");
+        asia.addTribeToContinent(homosepian);
+        asia.addTribeToContinent(neanderthal);
+        africa.addTribeToContinent(homoerectus);
+        africa.addTribeToContinent(homoebolis);
+        asia.addTribeToContinent(homofloresiensis);
+        Planet planet = Planet.getInstance();
+        planet.addContinentToPlanet(asia);
+        planet.addContinentToPlanet(africa);
+        planet.runPlanetCycle();
     }
 }
