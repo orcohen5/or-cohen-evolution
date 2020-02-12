e8dbe512bcf8535f88af80b6cd36976788d35d8d
אור מרד כהן <s8253446@idf.il>
2020-02-12 13:47:11 +0200

    refactor: removing class PlanetLifeCycle and uniting the class actions with Planet class due to architectural changes. feat: adding run function to Planet class since it extends TimerTask as it was in PlanetLifeCycle class. style: adding spaces for better reading.

--- src/main/java/Places/Planet.java
+++ src/main/java/Places/Planet.java
@@ -1,18 +1,21 @@
 package Places;
 
-import Events.PlanetLifeCycle;
-
 import java.util.ArrayList;
 import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Planet {
-
+public class Planet extends TimerTask {
+    private final int THREADS_NUMBER = 3;
     private static Planet instance = null;
     private ArrayList<Continent> continentsInPlanet;
+    private ExecutorService executor;
 
     private Planet() {
         System.out.println("Planet Started!");
         this.continentsInPlanet = new ArrayList<Continent>();
+        this.executor = Executors.newFixedThreadPool(THREADS_NUMBER);
     }
 
     public static Planet getInstance() {
@@ -23,10 +26,6 @@ public class Planet {
         return instance;
     }
 
-    public void addContinentToPlanet(Continent continent) {
-        this.continentsInPlanet.add(continent);
-    }
-
     public ArrayList<Continent> getContinentsInPlanet() {
         return continentsInPlanet;
     }
@@ -35,8 +34,29 @@ public class Planet {
         this.continentsInPlanet = continentsInPlanet;
     }
 
-    public void runPlanetCycle() {
+    public void addContinentToPlanet(Continent continent) {
+        this.continentsInPlanet.add(continent);
+    }
+
+    public void runCycle() {
+        printDivider();
+
+        for(int i = 0; i < this.continentsInPlanet.size(); i++) {
+            this.executor.execute(this.continentsInPlanet.get(i));
+        }
+    }
+
+    public void executeLifeCycleEverySecond() {
         Timer timer = new Timer();
-        timer.schedule(new PlanetLifeCycle(this), 0, 1000);
+        timer.schedule(this, 0, 1000);
+    }
+
+    public void printDivider() {
+        System.out.println("______________");
+    }
+
+
+    public void run() {
+        runCycle();
     }
 }
