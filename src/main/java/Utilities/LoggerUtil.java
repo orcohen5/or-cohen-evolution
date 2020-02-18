f957d21a0e61de27a485e7781e96fe3e590f9b54
אור מרד כהן <s8253446@idf.il>
2020-02-18 10:28:15 +0200

    feat adding new class which is responsible for log4j logging.

---
+++ src/main/java/Utilities/LoggerUtil.java
@@ -0,0 +1,13 @@
+package Utilities;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerUtil {
+    private static Logger logger = LogManager.getLogger(LoggerUtil.class);
+
+    public static void logData(String data) {
+        logger.info(data);
+    }
+    
+}
