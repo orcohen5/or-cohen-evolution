fb81fff15dfd43b58021b526d5b52c608fe03132
orcohen5 <or.125.1@gmail.com>
2020-03-17 05:50:12 +0200

    feat: adding more continents and organisms to the evolution. creating ScheduledExecutorService to replace Timer in EvolutionManager.

--- src/main/java/Events/EvolutionManager.java
+++ src/main/java/Events/EvolutionManager.java
@@ -3,40 +3,60 @@ package Events;
 import Organisms.*;
 import Places.Continent;
 import Places.Planet;
+import Utilities.ExecutorServiceUtil;
 
 import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class EvolutionManager {
     private Planet planet;
     private Continent asia;
     private Continent africa;
+    private Continent europe;
+    private Continent america;
     private List<Organism> asianOrganismsList;
     private List<Organism> africanOrganismsList;
+    private List<Organism> europeanOrganismsList;
+    private List<Organism> americanOrganismsList;
     private List<Continent> continentsList;
-    private Timer timer;
+    private ScheduledExecutorService startExecutor;
 
     public EvolutionManager() {
         this.asianOrganismsList = new ArrayList();
         this.africanOrganismsList = new ArrayList();
+        this.europeanOrganismsList = new ArrayList();
+        this.americanOrganismsList = new ArrayList();
         this.continentsList = new ArrayList();
+        this.startExecutor = ExecutorServiceUtil.getStartExecutor();
     }
 
     public void initializeEvolution() {
         asianOrganismsList.add(new Homosepian("Nadav"));
-        africanOrganismsList.add(new Neanderthal("Sapir"));
-        africanOrganismsList.add(new Homoerectus("Tal"));
         asianOrganismsList.add(new Homoebolis("Yehuda"));
-        africanOrganismsList.add(new Homosepian("Shalom"));
         asianOrganismsList.add(new Homofloresiensis("Or"));
+        africanOrganismsList.add(new Neanderthal("Demoza"));
+        africanOrganismsList.add(new Homoerectus("Benny"));
+        africanOrganismsList.add(new Homosepian("Zohar"));
+        europeanOrganismsList.add(new Homoerectus("Tal"));
+        europeanOrganismsList.add(new Neanderthal("Sapir"));
+        europeanOrganismsList.add(new Homofloresiensis("Netanel"));
+        americanOrganismsList.add(new Homosepian("Shalom"));
+        americanOrganismsList.add(new Neanderthal("Ori"));
+        americanOrganismsList.add(new Homoebolis("Bar"));
         asia = new Continent("Asia", asianOrganismsList);
         africa = new Continent("Africa", africanOrganismsList);
+        europe = new Continent("Europe", europeanOrganismsList);
+        america = new Continent("America", americanOrganismsList);
         continentsList.add(asia);
         continentsList.add(africa);
+        continentsList.add(europe);
+        continentsList.add(america);
         planet = Planet.getInstance(continentsList);
     }
 
     public void startEvolution() {
-        timer = new Timer();
-        timer.schedule(planet, 0, 1000);
+        startExecutor.scheduleAtFixedRate(planet, 0, 1, TimeUnit.SECONDS);
     }
 }
--- src/main/java/Utilities/ExecutorServiceUtil.java
+++ src/main/java/Utilities/ExecutorServiceUtil.java
@@ -2,12 +2,18 @@ package Utilities;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class ExecutorServiceUtil {
-    private static final int THREADS_NUMBER = 3;
+    private static final int THREADS_NUMBER = 5;
     private static ExecutorService executor = Executors.newFixedThreadPool(THREADS_NUMBER);
+    private static ScheduledExecutorService startExecutor = Executors.newSingleThreadScheduledExecutor();
 
     public static ExecutorService getExecutor() {
         return executor;
+    }
+
+    public static ScheduledExecutorService getStartExecutor() {
+        return startExecutor;
     }
 }
