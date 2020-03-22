51775c47d090ec87fba064ae35834d782631cafd
orcohen5 <or.125.1@gmail.com>
2020-03-22 02:26:03 +0200

    refactor: removing not needed function. build: reorganization of files in different packages.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-import Events.EvolutionManager;
+import org.iaf.evolution.EvolutionManager;
 
 public class Main {
     public static void main(String[] args) {
         EvolutionManager evolutionManager = new EvolutionManager();
         evolutionManager.initializeEvolution();
-        evolutionManager.startEvolution();
     }
+
 }
