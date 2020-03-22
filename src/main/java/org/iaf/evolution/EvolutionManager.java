09658086cc3615dea507ff658289ef3d9ec2ad76
orcohen5 <or.125.1@gmail.com>
2020-03-22 02:22:51 +0200

    feat: adding continents to planet after planet initialization instead of both at the same time. style: changing all of class variables to local variables except for executor. initializing planet in startEvolution. build: reorganization of files in different packages.

---
+++ src/main/java/org/iaf/evolution/EvolutionManager.java
@@ -0,0 +1,49 @@
+package org.iaf.evolution;
+
+import org.iaf.evolution.entities.organisms.*;
+import org.iaf.evolution.entities.Continent;
+import org.iaf.evolution.entities.Planet;
+import org.iaf.evolution.utilities.ExecutorServiceUtil;
+
+import java.util.*;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class EvolutionManager {
+    private ScheduledExecutorService executorScheduler;
+
+    public EvolutionManager() {
+        this.executorScheduler = ExecutorServiceUtil.getExecutorScheduler();
+    }
+
+    public void initializeEvolution() {
+        List<Organism> asianOrganismsList = new ArrayList();
+        List<Organism> africanOrganismsList = new ArrayList();
+        List<Organism> europeanOrganismsList = new ArrayList();
+        List<Organism> americanOrganismsList = new ArrayList();
+        List<Continent> continentsList = new ArrayList();
+        asianOrganismsList.add(new Homosepian("Nadav"));
+        asianOrganismsList.add(new Homoebolis("Yehuda"));
+        asianOrganismsList.add(new Homofloresiensis("Or"));
+        africanOrganismsList.add(new Neanderthal("Demoza"));
+        africanOrganismsList.add(new Homoerectus("Benny"));
+        africanOrganismsList.add(new Homosepian("Zohar"));
+        europeanOrganismsList.add(new Homoerectus("Tal"));
+        europeanOrganismsList.add(new Neanderthal("Sapir"));
+        europeanOrganismsList.add(new Homofloresiensis("Netanel"));
+        americanOrganismsList.add(new Homosepian("Shalom"));
+        americanOrganismsList.add(new Neanderthal("Ori"));
+        americanOrganismsList.add(new Homoebolis("Efi"));
+        continentsList.add(new Continent("Asia", asianOrganismsList));
+        continentsList.add(new Continent("Africa", africanOrganismsList));
+        continentsList.add(new Continent("Europe", europeanOrganismsList));
+        continentsList.add(new Continent("America", americanOrganismsList));
+        startEvolution(continentsList);
+    }
+
+    public void startEvolution(List<Continent> continentsList) {
+        Planet planet = Planet.getInstance();
+        planet.setContinents(continentsList);
+        executorScheduler.scheduleAtFixedRate(planet, 0, 1, TimeUnit.SECONDS);
+    }
+}
